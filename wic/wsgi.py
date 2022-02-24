7bfeba8b92d80c7526a8952bbe707f563af29939
David Hale <davidhale87@gmail.com>
2022-02-23 23:20:19 -0600

    trying to get enviromental variables to work

--- wic/wsgi.py
+++ wic/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for wic project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wic.settings')
-
+project_folder = os.path.expanduser('~/wic.flyhomemn.com/wic/.env')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
 application = get_wsgi_application()
