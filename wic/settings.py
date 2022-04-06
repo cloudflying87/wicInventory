c0c6243b346c48a7369c6cb930a5775ad1282067
David Hale <davidhale87@gmail.com>
2022-04-05 22:28:32 -0500

    trying to get the css sheets to be found

--- wic/settings.py
+++ wic/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['wic.flyhomemn.com','localhost']
 
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
