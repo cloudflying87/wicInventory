4a4c695ee34ea6d43f714c7ebe6d0eebf9c0595b
David Hale <davidhale87@gmail.com>
2022-04-06 10:24:11 -0500

    Trying whitenoise

--- wic/settings.py
+++ wic/settings.py
@@ -4,22 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['wic.flyhomemn.com','localhost']
-
-# Application definition
 
 INSTALLED_APPS = [
     'inventory',
@@ -43,6 +34,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_currentuser.middleware.ThreadLocalUserMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wic.urls'
@@ -66,8 +58,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'wic.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -81,8 +71,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -99,10 +87,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -112,15 +96,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_ROOT = '/home/flyhomem/wic.flyhomemn.com/staticcollection'
 STATIC_URL = '/staticcollection/'
 STATICFILES_DIRS = [
     "/home/flyhomem/virtualenv/wic.flyhomemn.com/3.8/lib/python3.8/site-packages/",
     '/home/flyhomem/wic.flyhomemn.com/static',
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticcollection')
     
     ]
 
